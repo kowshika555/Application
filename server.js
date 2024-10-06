281d962a4461541f30cc25c3b2b98984749b813b
kowshika555 <kowshika223@gmail.com>
2024-10-07 00:42:08 +0530

    change server

--- server.js
+++ server.js
@@ -5,45 +5,32 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Create connection to MySQL database
+// Connect to MySQL Database
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'root', // use your MySQL username
-  password: '', // use your MySQL password
+  user: 'root',    // MySQL username
+  password: '',    // MySQL password
   database: 'simple_website'
 });
 
-// Connect to the database
-db.connect(err => {
+// Establish the MySQL connection
+db.connect((err) => {
   if (err) {
     throw err;
   }
   console.log('MySQL Connected...');
 });
 
-// Create a database table for users
-app.get('/createdb', (req, res) => {
-  let sql = 'CREATE DATABASE IF NOT EXISTS simple_website';
-  db.query(sql, err => {
-    if (err) throw err;
-    res.send('Database created...');
-  });
-});
-
-// Create users table
-app.get('/createtable', (req, res) => {
-  let sql = 'CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, username VARCHAR(255), password VARCHAR(255), PRIMARY KEY(id))';
-  db.query(sql, err => {
-    if (err) throw err;
-    res.send('Users table created...');
-  });
+// Route for home
+app.get('/', (req, res) => {
+  res.send('Welcome to Simple Website!');
 });
 
 // Handle user registration
 app.post('/register', (req, res) => {
   let user = { username: req.body.username, password: req.body.password };
   let sql = 'INSERT INTO users SET ?';
-  db.query(sql, user, err => {
+  db.query(sql, user, (err, result) => {
     if (err) throw err;
     res.send('User registered...');
   });
@@ -59,12 +46,12 @@ app.post('/login', (req, res) => {
     if (result.length > 0) {
       res.send('Login successful!');
     } else {
-      res.send('Invalid username or password');
+      res.send('Invalid credentials');
     }
   });
 });
 
-// Start server
+// Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
